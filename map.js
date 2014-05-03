210a8d15966c68088aea81f1a1a270cbeebe270f
Ruan Costa <ruan.cocito@gmail.com>
2014-05-03 09:59:11 -0300

    som bolado nao funcionou

--- map.js
+++ map.js
@@ -21,87 +21,26 @@ var canceled = false;
 var button_wake;
 var button_stop;
 
-/*function html5_audio()
-{
-  var a = document.createElement('audio');
-  return !!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''));
-}
- 
-var play_html5_audio = false;
-if(html5_audio()) play_html5_audio = true;
- 
-function play_sound(url)
-{
-  if(play_html5_audio){
-    var snd = new Audio(url);
-    snd.load();
-    snd.play();
-  }
-  else
-  {
-    $("#sound").remove();
-    var sound = $("<embed id='sound' type='audio/mpeg' />");
-    sound.attr('src', url);
-    sound.attr('loop', false);
-    sound.attr('hidden', true);
-    sound.attr('autostart', true);
-    $('body').append(sound);
-  }
-}*/
-
-  function html5_audio()
-  {
-    var a = document.createElement('audio');
-    return !!(a.canPlayType && a.canPlayType('audio/mpeg;').replace(/no/, ''));
-  }
- 
-  var play_html5_audio = false;
-  if(html5_audio()) play_html5_audio = true;
- 
-  function play_sound(url)
-  {
-    if(play_html5_audio)
-    {
-      var snd = new Audio(url);
-      snd.load();
-      snd.play();
-    }
-    else
-    {
-      $("#sound").remove();
-      var sound = $("<embed id='sound' type='audio/mpeg' />");
-      sound.attr('src', url);
-      sound.attr('loop', false);
-      sound.attr('hidden', true);
-      sound.attr('autostart', true);
-      $('body').append(sound);
-    }
-  }
-
 /**
  * function: initialize
  * Inicializa um novo mapa para ser usado pelo usuário.
  */
 function initialize() 
 {
-  play_sound('default.mp3');
-  //audio = document.getElementById("audio");
-  //if(typeof(audio) == "undefined")
-  //  console.log("AUEHAUEHUAHEUAHUEHAUHEUAHUEHAUEHAU");
+  audio = document.getElementById("audio");
   button_wake = document.getElementById("button_wake");
   button_stop = document.getElementById("button_stop");
   button_canc = document.getElementById("button_canc");
   button_stop.style.display="none";
   button_canc.style.display="none";
-  //audio.play();
+  audio.play();
   
   // Initial map
   var mapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     zoom: 16
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   // Browser doesn't support Geolocation
   if(navigator.geolocation) geoUpdate();
