388410bcd3e211076871a628e82dc7562c6ebb35
Renato Silva Bispo <rsbispo962@hotmail.com>
2014-05-03 12:12:57 -0300

    Icon

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Geolocation</title>
+    <title>waker</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
