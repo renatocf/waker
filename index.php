0901cd9244be86a58315fed3611e29ca36a1a6b1
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2014-05-03 13:36:00 -0300

    Modularizing and adding new logo

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="map.css" rel="stylesheet" type="text/css">
+    
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/icon">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/map.css" rel="stylesheet" type="text/css">
 
     <!-- //////////////////////////////////// API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ -->
     <!--
@@ -14,37 +16,39 @@
     sensor (a GPS locator) to determine the user's location.
     See: https://developers.google.com/maps/documentation/javascript/tutorial#Loading_the_Maps_API
     -->
-    <audio id="audio" src="default.mp3"></audio>
+    <audio id="audio" src="data/default.mp3"></audio>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&libraries=places,geometry"></script>
   
-    <!-- //////////////////////////////// Our scripts \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ -->
-    <script type="text/javascript" src="map.js"></script>
+    <!-- /////////////////////////////// INTERNAL STRIPTS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ -->
+    <script type="text/javascript" src="js/map.js"></script>
   </head>
   
   <body>
     <!-- /// TOPBAR \\\-->
     <nav class="navbar navbar-inverse" role="navigation">
-      <a href="index.html"><img id="title"  src="title.png"></a>
+      <a href="index.php"><img id="title"  src="img/title.png"></a>
       <div class="container-fluid">
         <div class="navbar-header">
         </div>
       </div>
     </nav>
-    
+
+    <!-- Input -->
     <div class="input-group">
-      <span class="input-group-addon"><img src="map-marker.png" height="20" width="20"></span>
+      <span class="input-group-addon"><img src="img/map-marker.png" height="20" width="20"></span>
       <input id="pac-input" type="text" class="form-control" placeholder="Where are you going?">
     </div>
     
+    <!-- Map -->
     <div id="map-canvas"></div>
     
+    <!-- Buttons -->
     <div id = "button_wake" class="btn btn-lg btn-primary" role="button" onclick="geoUpdateR()" >Wake me!</div>
     <div id = "button_stop" class="btn btn-lg btn-danger"  role="button" onclick="stop()"       >Stop!</div>
     <div id = "button_canc" class="btn btn-lg btn-warning" role="button" onclick="cancel()"     >Cancel</div>
     
     <!-- Bootstrap core JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="bootstrap.js"></script>
+    <script src="js/bootstrap.js"></script>
   </body>
-
 </html>
